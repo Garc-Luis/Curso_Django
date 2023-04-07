bd770014e1ec3d4d80e1c6788d0cdb705ece2d79
Luis Alberto Garcia Corona <garcialuis080918@gmail.com>
2023-04-07 18:27:51 -0300

    Herencia

--- Herencia/Herencia/views.py
+++ Herencia/Herencia/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def herencia(request):
     return render(request, 'herencia.html', {})
+
+def ejemplo(request):
+    return render(request, 'ejemplo.html', {})
+
+def otra(request):
+    return render(request, 'otra.html', {})
