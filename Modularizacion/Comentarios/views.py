cc7d37605879105040f6149528b3587045f95a2d
Luis Alberto Garcia Corona <garcialuis080918@gmail.com>
2023-04-09 16:16:41 -0300

    Modularizacion en Django

--- Modularizacion/Comentarios/views.py
+++ Modularizacion/Comentarios/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def test(request):
+    return HttpResponse('Funciona Correctamente')
+
+def create(request):
+    return HttpResponse('Ruta para probar la Creacion de Modelos')
